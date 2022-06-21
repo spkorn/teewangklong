3845b73fa94d27667f5332ef0462a7ac1fe5da8d
spkorn <s_meelarp@hotmail.com>
2022-06-21 15:51:58 +0700

    feat: add homepage-banner

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,11 +1,12 @@
 //import "./App.css";
-import "./components/NavHeader";
+import Banner from "./components/HomePage/Banner";
 import NavHeader from "./components/NavHeader";
 
 function App() {
   return (
     <div className="App">
       <NavHeader />
+      <Banner />
     </div>
   );
 }
--- webpage/src/photos/images.js
+++ webpage/src/photos/images.js
@@ -1,7 +1,11 @@
 import logo from "./logo.jpeg";
+import banner from "./banner.jpeg";
+import banner1 from "./banner1.jpeg";
 
 const image = {
   logo,
+  banner,
+  banner1,
 };
 
 export default image;
